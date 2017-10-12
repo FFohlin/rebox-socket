eb614c88ea470fa4deff9efd916a0fb3afc71387
Fredric Fohlin <fredric@fohlin.se>
2017-10-12 14:03:05 +0200

    first code commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var app = require('express')();
+var bodyParser  = require('body-parser');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+let portNumber = 3001;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(bodyParser.json());
+
+app.post('/', function(req, res){
+  //if(res.socket.remoteAddress == '127.0.0.1') {
+      // The server is trying to send us an activity message
+      res.writeHead(200, [
+        ["Content-Type", "text/plain"],
+        ["Content-Length", 0]
+      ]);
+      res.write('');
+      res.end();
+       if (req.body) {
+         io.emit('public data changes', req.body);
+       }
+  //}
+});
+
+io.on('connection', function(socket){
+  /*
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  */
+  /*
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  */
+});
+
+// Om man vill skicka till alla utom sig själv...
+//io.on('connection', function(socket){
+//  socket.broadcast.emit('hi');
+//});
+
+io.on('connection', function(socket){
+  socket.on('public data changes', function(msg){
+    io.emit('public data changes', msg);
+  });
+});
+
+http.listen(portNumber, function(){
+  console.log('listening on *:3001');
+});
