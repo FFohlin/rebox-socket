66b40f85f75d6e537c2c2e5b18471a8801b5ff6a
Fredric Fohlin <fredric@fohlin.se>
2017-11-06 21:11:27 +0100

    Update start.js

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const bodyParser  = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-//const ipaddress = process.env.OPENSHIFT_IP || null;
+const ipaddress = process.env.OPENSHIFT_IP || null;
 const portNumber = process.env.PORT || process.env.OPENSHIFT_PORT || 3001;
 
 //const portNumber = 3001;
@@ -40,6 +40,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(portNumber, function(){
-  console.log('listening on *:' + portNumber);
+http.listen(portNumber, ipaddress, function(){
+  console.log('listening on ' + ipaddress + ':' + portNumber);
 });
